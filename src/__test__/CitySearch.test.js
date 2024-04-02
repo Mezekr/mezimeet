d61431073f41d4fa22df4f1ce6dc0f970c04e266
Mezekr W <mezi.edu@tuta.io>
2024-04-02 21:28:36 +0200

    write test to check element with role textbox

---
+++ src/__test__/CitySearch.test.js
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import CitySearch from '../components/CitySearch';
+
+describe('<CitySearch /> component', () => {
+	test('renders text input', () => {
+		const CitySearchComponent = render(<CitySearch />);
+		const cityTextBox = CitySearchComponent.queryByRole('textbox');
+		expect(cityTextBox).toBeInTheDocument();
+		expect(cityTextBox).toHaveClass('city');
+	});
+});
