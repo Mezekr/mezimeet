037e9bb444ca8c4f32770869ddb0c4a47b7b77aa
Mezekr W <mezi.edu@tuta.io>
2024-04-02 20:38:16 +0200

    write test for checking element with listitem role

--- src/__test__/EventList.test.js
+++ src/__test__/EventList.test.js
@@ -3,8 +3,10 @@ import React from 'react';
 import EventList from '../components/EventList';
 
 describe('<EventList /> component', () => {
-	test('has an element with "list" role', () => {
-		const EventListComponent = render(<EventList />);
-		expect(EventListComponent.queryByRole('list')).toBeInTheDocument();
+	test('renders correct number of events', () => {
+		const EventListComponent = render(
+			<EventList events={[{}, {}, {}, {}]} />
+		);
+		expect(EventListComponent.getAllByRole('listitem')).toHaveLength(4);
 	});
 });
