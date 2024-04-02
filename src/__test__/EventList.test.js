e98646be37ba449a60c74c163bf554a46dafa5d9
Mezekr W <mezi.edu@tuta.io>
2024-04-02 21:16:53 +0200

    refactor tests with the beforeEach Jest feature

--- src/__test__/EventList.test.js
+++ src/__test__/EventList.test.js
@@ -3,8 +3,17 @@ import React from 'react';
 import EventList from '../components/EventList';
 
 describe('<EventList /> component', () => {
+	let EventListComponent;
+	beforeEach(() => {
+		EventListComponent = render(<EventList />);
+	});
+
+	test('has an element with "list" role', () => {
+		expect(EventListComponent.queryByRole('list')).toBeInTheDocument();
+	});
+
 	test('renders correct number of events', () => {
-		const EventListComponent = render(
+		EventListComponent.rerender(
 			<EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />
 		);
 		expect(EventListComponent.getAllByRole('listitem')).toHaveLength(4);
