bf6d15cef9362f91ca692ea3d995758fb7c16dee
Mezekr W <mezi.edu@tuta.io>
2024-04-02 20:03:53 +0200

    add EventList component to list events

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './App.css';
-
+import EventList from './components/EventList';
 const App = () => {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<EventList />
+		</div>
+	);
 };
 
 export default App;
