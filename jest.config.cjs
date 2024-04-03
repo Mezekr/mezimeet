14154fe3a796c77c8c90572d82c78b8c9adeca4c
Mezekr W <mezi.edu@tuta.io>
2024-04-03 20:50:04 +0200

    update jest config files

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,11 +4,5 @@ module.exports = {
 	transform: {
 		'^.+\\.(js|jsx|ts|tsx|mjs)$': 'babel-jest',
 	},
-	setupFilesAfterEnv: ['<rootDir>/src/jest-setup.js'],
+	setupFilesAfterEnv: ['./src/jest-setup.js'],
 };
-
-// testEnvironment: 'jsdom',
-// 	transformIgnorePatterns: ['node_modules/(?!(sucrase)/)'],
-// 	transform: {
-// 		'^.+\\.(js|jsx|ts|tsx|mjs)$': 'babel-jest',
-// 	},
