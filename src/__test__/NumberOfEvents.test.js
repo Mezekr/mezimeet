7bc18e13f0961fba26d480e2ce952f8172676791
Mezekr W <mezi.edu@tuta.io>
2024-04-03 21:02:08 +0200

    add test to check NumberOfEvents textbox element

---
+++ src/__test__/NumberOfEvents.test.js
@@ -0,0 +1,16 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import NumberOfEvents from '../components/NumberOfEvents';
+
+describe('<NumberOfEvents/> component', () => {
+	let NumberOfEventsComponent;
+	beforeEach(() => {
+		NumberOfEventsComponent = render(<NumberOfEvents />);
+	});
+
+	test('has an element with "textbox" role', () => {
+		expect(
+			NumberOfEventsComponent.queryByRole('textbox')
+		).toBeInTheDocument();
+	});
+});
