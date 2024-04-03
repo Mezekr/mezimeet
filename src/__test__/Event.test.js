dad02f513c8922df23910985de46bd5e181c6ffc
Mezekr W <mezi.edu@tuta.io>
2024-04-03 19:13:46 +0200

    add test to show details if user clicks

--- src/__test__/Event.test.js
+++ src/__test__/Event.test.js
@@ -1,4 +1,5 @@
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { getEvents } from '../api';
 import Event from '../components/Event';
@@ -19,22 +20,34 @@ describe('<Event/>', () => {
 			EventComponent.queryByText(allEvents[0].summary)
 		).toBeInTheDocument();
 	});
+
 	test('render event start time', () => {
 		expect(
 			EventComponent.queryByText(allEvents[0].created)
 		).toBeInTheDocument();
 	});
+
 	test('render event location', () => {
 		expect(
 			EventComponent.queryByText(allEvents[0].location)
 		).toBeInTheDocument();
 	});
+
 	test('render event details button with title (show details)', () => {
 		expect(EventComponent.queryByText(/show details/i)).toBeInTheDocument();
 	});
+
 	test('by default, the Events Details section should be hidden', () => {
 		expect(
 			EventComponent.container.querySelector('.details')
 		).not.toBeInTheDocument();
+	});
+
+	test('shows details details section when the user clicks the "Show details" button', async () => {
+		const user = userEvent.setup();
+		const detailsBtn = EventComponent.queryByRole('button');
+		await user.click(detailsBtn, 'Show Details');
+		const details = EventComponent.container.querySelector('.details');
+		expect(details).toBeInTheDocument();
 	});
 });
