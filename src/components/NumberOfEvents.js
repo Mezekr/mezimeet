6623da63dd37c2754b2bc91c9bf1843948f36832
Mezekr W <mezi.edu@tuta.io>
2024-04-03 21:04:19 +0200

    pass test by adding NumberOfEvents textbox element

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,6 +1,16 @@
 import React from 'react';
 
 const NumberOfEvents = () => {
-	return <div id="number-of-events"></div>;
+	return (
+		<div id="number-of-events">
+			<input
+				type="text"
+				value={''}
+				placeholder="Number of Event"
+				className="numberOfEvents"
+			/>
+			;
+		</div>
+	);
 };
 export default NumberOfEvents;
