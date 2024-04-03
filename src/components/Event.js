e57dff7d9c02099c0b17ccbeb6c5cff4b316d73c
Mezekr W <mezi.edu@tuta.io>
2024-04-03 18:54:17 +0200

    pass test with default hiding of event details

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Event = ({ event }) => {
+	const [showDetails, setShowDetails] = useState(false);
 	return (
-		<li>
+		<li className="event" key={event.id}>
 			<h2>{event.summary}</h2>
 			<p>{event.created}</p>
 			<p>{event.location}</p>
-			<button>show details</button>
+			<button
+				className="showDetailsBtn"
+				onClick={() => setShowDetails(!showDetails)}
+			>
+				{showDetails ? 'Hide Details' : 'Show Details'}
+			</button>
+
+			{/* details section */}
+			{showDetails ? (
+				<div className="detals">
+					<h3>Event Details</h3>
+					<p> {event.kind}</p>
+					<p> {event.description}</p>
+					<p> {event.status}</p>
+				</div>
+			) : null}
 		</li>
 	);
 };
