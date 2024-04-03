fedf5122b5e2f9e2b764583cc00c3b01e9b7641f
Mezekr W <mezi.edu@tuta.io>
2024-04-03 17:48:45 +0200

    refactor using beforAll & beforEach Jest functions

--- src/__test__/Event.test.js
+++ src/__test__/Event.test.js
@@ -5,34 +5,31 @@ import Event from '../components/Event';
 
 describe('<Event/>', () => {
 	let EventComponent;
+	let allEvents;
+
+	beforeAll(async () => {
+		allEvents = await getEvents();
+	});
 	beforeEach(() => {
-		EventComponent = render('< Event />');
+		EventComponent = render(<Event event={allEvents[0]} />);
 	});
 
-	test('render event title', async () => {
-		const allEvents = await getEvents();
-		EventComponent.rerender(<Event event={allEvents[0]} />);
+	test('render event title', () => {
 		expect(
 			EventComponent.queryByText(allEvents[0].summary)
 		).toBeInTheDocument();
 	});
-	test('render event start time', async () => {
-		const allEvents = await getEvents();
-		EventComponent.rerender(<Event event={allEvents[0]} />);
+	test('render event start time', () => {
 		expect(
 			EventComponent.queryByText(allEvents[0].created)
 		).toBeInTheDocument();
 	});
-	test('render event location', async () => {
-		const allEvents = await getEvents();
-		EventComponent.rerender(<Event event={allEvents[0]} />);
+	test('render event location', () => {
 		expect(
 			EventComponent.queryByText(allEvents[0].location)
 		).toBeInTheDocument();
 	});
-	test('render event details button with title (show details)', async () => {
-		const allEvents = await getEvents();
-		EventComponent.rerender(<Event event={allEvents[0]} />);
+	test('render event details button with title (show details)', () => {
 		expect(EventComponent.queryByText(/show details/i)).toBeInTheDocument();
 	});
 });
