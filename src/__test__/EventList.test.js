b9399e9e4135b72d617c4634428bbfecb7463601
Mezekr W <mezi.edu@tuta.io>
2024-04-05 08:57:20 +0200

    add filter events by city feature

--- src/__test__/EventList.test.js
+++ src/__test__/EventList.test.js
@@ -1,5 +1,6 @@
-import { render } from '@testing-library/react';
+import { render, waitFor, within } from '@testing-library/react';
 import React from 'react';
+import App from '../App.jsx';
 import { getEvents } from '../api';
 import EventList from '../components/EventList';
 
@@ -19,5 +20,18 @@ describe('<EventList /> component', () => {
 		expect(EventListComponent.getAllByRole('listitem')).toHaveLength(
 			allEvents.length
 		);
+	});
+});
+
+describe('<EventList /> integration', () => {
+	test('renders a list of 32 events when the app is mounted and rendered', async () => {
+		const AppComponent = render(<App />);
+		const AppDOM = AppComponent.container.firstChild;
+		const EventListDOM = AppDOM.querySelector('#event-list');
+		await waitFor(() => {
+			const EventListItems =
+				within(EventListDOM).queryAllByRole('listitem');
+			expect(EventListItems.length).toBe(32);
+		});
 	});
 });
