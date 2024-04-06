2705bfee4ee901e8d04bb8c699b41668cc4d2d97
Mezekr W <mezi.edu@tuta.io>
2024-04-06 10:39:42 +0200

    add test to update NumberOfEvents to user input

--- src/__test__/NumberOfEvents.test.js
+++ src/__test__/NumberOfEvents.test.js
@@ -1,11 +1,15 @@
-import { render } from '@testing-library/react';
+import { render, waitFor, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
+import App from '../App.jsx';
 import NumberOfEvents from '../components/NumberOfEvents';
+
 describe('<NumberOfEvents/> component', () => {
 	let NumberOfEventsComponent;
 	beforeEach(() => {
-		NumberOfEventsComponent = render(<NumberOfEvents />);
+		NumberOfEventsComponent = render(
+			<NumberOfEvents setCurrentNOE={() => {}} />
+		);
 	});
 
 	test('has an element with "textbox" role', () => {
@@ -25,5 +29,27 @@ describe('<NumberOfEvents/> component', () => {
 		const user = userEvent.setup();
 		await user.type(numberOfEvents, '{backspace}{backspace}10');
 		expect(numberOfEvents).toHaveValue('10');
+	});
+});
+
+describe('<NumberOfEvents/> integration', () => {
+	test('number of events updated correctly to user specified number of events', async () => {
+		const user = userEvent.setup();
+		const AppComponent = render(<App />);
+		const AppDOM = AppComponent.container.firstChild;
+
+		const NumberOfEventsComponent =
+			AppDOM.querySelector('#number-of-events');
+		const numberOfEventsTextBox = within(
+			NumberOfEventsComponent
+		).queryByRole('textbox');
+		await user.type(numberOfEventsTextBox, '{backspace}{backspace}10');
+		expect(numberOfEventsTextBox).toHaveValue('10');
+		const EventListDOM = AppDOM.querySelector('#event-list');
+		await waitFor(() => {
+			const EventListItems =
+				within(EventListDOM).queryAllByRole('listitem');
+			expect(EventListItems.length).toBe(10);
+		});
 	});
 });
