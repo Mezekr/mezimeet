5926539fd40e57d9d5be21bc5ba1588a499611dc
Mezekr W <mezi.edu@tuta.io>
2024-04-14 09:57:22 +0200

    add functionality to load cached event data

--- src/api.js
+++ src/api.js
@@ -12,6 +12,11 @@ export const getEvents = async () => {
 	if (window.location.href.startsWith('http://localhost')) {
 		return mockData;
 	}
+	if (!navigator.onLine) {
+		const events = localStorage.getItem('lastEvents');
+		NProgress.done();
+		return events ? JSON.parse(events) : [];
+	}
 	const token = await getAccessToken();
 	if (token) {
 		removeQuery();
@@ -22,6 +27,8 @@ export const getEvents = async () => {
 		const response = await fetch(url);
 		const result = await response.json();
 		if (result) {
+			NProgress.done();
+			localStorage.setItem('lastEvents', JSON.stringify(result.events));
 			return result.events;
 		} else return null;
 	}
